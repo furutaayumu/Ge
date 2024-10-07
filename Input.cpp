8219d871e8c9a3fc6ea2cd46f7e96f907193affd
furutaayumu <k023g0086@g.neec.ac.jp>
2024-10-07 16:10:16 +0900

    02_01終わり

---
+++ Input.cpp
@@ -0,0 +1,31 @@
+#include "Input.h"
+#include <dinput.h>
+#include <cassert>
+
+void Input::Initialize(HINSTANCE hInstance, HWND hwnd) {
+	HRESULT result;
+
+	//#Directinpuの初期化
+	IDirectInput8* directInput = nullptr;
+	result = DirectInput8Create(
+		hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8,
+		(void**)&directInput, nullptr);
+	assert(SUCCEEDED(result));
+
+	//キーボードデバイスの初期化
+	IDirectInputDevice8* keyboard = nullptr;
+	result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+	assert(SUCCEEDED(result));
+
+	//入力データ形式のセット
+	result = keyboard->SetDataFormat(&c_dfDIKeyboard);
+	assert(SUCCEEDED(result));
+	//排他制御レベルのセット
+	result = keyboard->SetCooperativeLevel(
+		hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+	assert(SUCCEEDED(result));
+}
+
+void Input::Updata() {
+
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
+#include "Input.h"
+
 
 #include<Windows.h>
 #include<cstdint>
@@ -526,25 +528,11 @@ int WINAPI	WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	Log("Complete create D3D12Device!!!\n");
 
-//#Directinpuの初期化
-	IDirectInput8* directInput = nullptr;
-	hr = DirectInput8Create(
-	wc.hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8,
-		(void**)&directInput, nullptr);
-	assert(SUCCEEDED(hr));
+	Input* input = nullptr;
 
-	//キーボードデバイスの初期化
-	IDirectInputDevice8* keyboard = nullptr;
-	hr = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
-	assert(SUCCEEDED(hr));
-
-	//入力データ形式のセット
-	hr = keyboard->SetDataFormat(&c_dfDIKeyboard);
-	assert(SUCCEEDED(hr));
-	//排他制御レベルのセット
-	hr = keyboard->SetCooperativeLevel(
-		hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
-	assert(SUCCEEDED(hr));
+	input = new Input();
+	input->Initialize(wc.hInstance,hwnd);
+	delete input;
 
 	//テクスチャ
 	DirectX::ScratchImage mipImages = LoadTexture("resources/uvChecker.png");
